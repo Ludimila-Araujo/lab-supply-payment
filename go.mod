26ee05d7492f5732a1d11ff339dc3494dfed83c3
Ludimila-Araujo <dearaujo.ludimila@gmail.com>
2026-08-26 14:21:08 -0300

    feat: add postgres configuration

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Ludimila-Araujo/labsupply-payment
 go 1.26.4
 
 require github.com/google/uuid v1.6.0
+
+require github.com/lib/pq v1.12.3
---
+++ internal/database/postgres.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+
+	"github.com/Ludimila-Araujo/labsupply-payment/internal/config"
+)
+
+func Connect(cfg config.Config) (*sql.DB, error) {
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
